b4cc5e9b991770ca59290fa481c0dcbe796c5c24
mawekk <mariamya243@gmail.com>
2022-04-23 21:45:58 +0300

    add print

--- src/main/kotlin/homeworks/homework3/AVLNode.kt
+++ src/main/kotlin/homeworks/homework3/AVLNode.kt
@@ -10,45 +10,50 @@ class AVLNode<K : Comparable<K>, V>(override var key: K, override var value: V) 
     }
 
     var leftChild: AVLNode<K, V>? = null
-        private set
     var rightChild: AVLNode<K, V>? = null
-        private set
-    var height = 0
-        private set
+    var height = 1
 
     override fun setValue(newValue: V): V {
         val oldValue = value
+
         value = newValue
+
         return oldValue
     }
 
     private fun getBalanceFactor(): Int {
         val leftHeight = leftChild?.height ?: 0
         val rightHeight = rightChild?.height ?: 0
+
         return rightHeight - leftHeight
     }
 
     private fun updateHeight() {
         val leftHeight = leftChild?.height ?: 0
         val rightHeight = rightChild?.height ?: 0
+
         height = max(leftHeight, rightHeight) + 1
     }
 
     private fun rotateRight(): AVLNode<K, V> {
-        val node = leftChild ?: throw IllegalArgumentException("Impossible to make right rotation")
+        val node = leftChild ?: throw IllegalArgumentException("impossible to make right rotation")
+
         leftChild = node.rightChild
         node.rightChild = this
         updateHeight()
         node.updateHeight()
+
         return node
     }
 
     private fun rotateLeft(): AVLNode<K, V> {
-        val node = rightChild ?: throw IllegalArgumentException("Impossible to make left rotation")
+        val node = rightChild ?: throw IllegalArgumentException("impossible to make left rotation")
+
         rightChild = node.leftChild
-        node.rightChild = this
-        node.updateHeight()
+        node.leftChild = this
         updateHeight()
+        node.updateHeight()
+
         return node
     }
 
@@ -83,23 +88,30 @@ class AVLNode<K : Comparable<K>, V>(override var key: K, override var value: V) 
         return this.leftChild?.findMinNode()
     }
 
-    private fun removeMinNode(): AVLNode<K, V> {
-        this.leftChild?.removeMinNode()
-        return this.balance()
+    private fun removeMinNode(): AVLNode<K, V>? {
+        if (leftChild == null)
+            return rightChild
+        leftChild = leftChild?.removeMinNode()
+        return balance()
     }
 
     fun removeNode(key: K): AVLNode<K, V>? {
-        return when (key.compareTo(key)) {
+        when (key.compareTo(key)) {
             1 -> rightChild?.removeNode(key)?.balance()
             -1 -> leftChild?.removeNode(key)?.balance()
             else -> {
                 if (rightChild == null)
-                    leftChild?.balance()
+                    return leftChild
                 val minimum = rightChild?.findMinNode()
                 minimum?.rightChild = rightChild?.removeMinNode()
                 minimum?.leftChild = leftChild
                 return minimum?.balance()
             }
         }
+        return balance()
+    }
+
+    override fun toString(): String {
+        return key.toString() + " -> " + value.toString()
     }
 }
--- src/main/kotlin/homeworks/homework3/AVLTree.kt
+++ src/main/kotlin/homeworks/homework3/AVLTree.kt
@@ -1,17 +1,24 @@
 package homeworks.homework3
 
+import kotlin.math.pow
+
 class AVLTree<K : Comparable<K>, V> : MutableMap<K, V> {
-    override var size: Int = 0
-        private set
     private var root: AVLNode<K, V>? = null
 
-    override val entries: MutableSet<MutableMap.MutableEntry<K, V>> = iterator().asSequence().toMutableSet()
-    override val keys: MutableSet<K> = iterator().asSequence().map { it.key }.toMutableSet()
-    override val values: MutableCollection<V> = iterator().asSequence().map { it.value }.toMutableSet()
+    override var entries: MutableSet<MutableMap.MutableEntry<K, V>> = mutableSetOf()
+    override val keys: MutableSet<K> = mutableSetOf()
+    override val values: MutableCollection<V> = mutableSetOf()
+    override var size: Int = 0
+        private set
 
-    override fun containsKey(key: K): Boolean = keys.contains(key)
+    override fun containsKey(key: K): Boolean {
+        return root?.findNode(key) != null
+    }
 
-    override fun containsValue(value: V): Boolean = values.contains(value)
+    override fun containsValue(value: V): Boolean {
+        entries = traverse(root)
+        return entries.find { it.value == value } != null
+    }
 
     override fun get(key: K): V? = root?.findNode(key)?.value
 
@@ -22,18 +29,21 @@ class AVLTree<K : Comparable<K>, V> : MutableMap<K, V> {
     }
 
     private fun putNode(node: AVLNode<K, V>?, key: K, value: V): AVLNode<K, V> {
-        return if (node == null) {
-            AVLNode(key, value).balance()
+        if (node == null) {
+            size++
+            return AVLNode(key, value)
         } else
             when (key.compareTo(node.key)) {
-                1 -> putNode(node.rightChild, key, value).balance()
-                -1 -> putNode(node.leftChild, key, value).balance()
-                else -> node.balance()
+                1 -> node.rightChild = putNode(node.rightChild, key, value)
+                -1 -> node.leftChild = putNode(node.leftChild, key, value)
+                else -> node.value = value
             }
+        return node.balance()
     }
 
     override fun put(key: K, value: V): V? {
-        root = putNode(root, key, value)
+        root = putNode(root, key, value).balance()
+
         return root?.value
     }
 
@@ -42,6 +52,64 @@ class AVLTree<K : Comparable<K>, V> : MutableMap<K, V> {
     }
 
     override fun remove(key: K): V? {
+        size--
+
         return root?.removeNode(key)?.value
+    }
+
+    private fun traverse(node: AVLNode<K, V>?): MutableSet<MutableMap.MutableEntry<K, V>> {
+        val set = mutableSetOf<MutableMap.MutableEntry<K, V>>()
+
+        return if (node != null) {
+            val left = node.leftChild
+            val right = node.rightChild
+
+            if (left != null)
+                set.addAll(traverse(left))
+            set.add(node)
+            if (right != null)
+                set.addAll(traverse(right))
+            set
+        } else mutableSetOf()
+    }
+
+    private fun splitIntoStrings(
+        node: AVLNode<K, V>?,
+        level: Int,
+        list: MutableList<MutableList<String>>
+    ): MutableList<MutableList<String>> {
+        if (node != null) {
+            list[level].add(node.toString())
+            splitIntoStrings(node.leftChild, level + 1, list)
+            splitIntoStrings(node.rightChild, level + 1, list)
+        } else
+            list[level].add("_")
+        return list
+    }
+
+    fun printTree() {
+        val list = mutableListOf<MutableList<String>>()
+        val height = root?.height ?: throw IllegalArgumentException("nothing to print: tree is empty")
+        for (i in 0..height)
+            list.add(mutableListOf())
+        val strings = splitIntoStrings(root, 0, list)
+        strings.removeLast()
+        var length = 0
+
+        entries = traverse(root)
+        entries.forEach {
+            if (it.value.toString().length > length)
+                length = it.value.toString().length
+        }
+        length += 5
+        for (i in 0 until strings.size) {
+            var indent = " ".repeat(length * (2.0.pow(height - i - 1) - 1).toInt())
+            print(indent)
+            for (string in strings[i]) {
+                indent = " ".repeat(length * (2.0.pow(height - i) - 1).toInt())
+                print(string.padEnd(length).replace("_", "") + indent)
+            }
+            println()
+        }
     }
 }
