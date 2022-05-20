8e744ef981fd8b1c14ee3cf316a1afd2fff44afe
mawekk <mariamya243@gmail.com>
2022-05-21 00:11:54 +0300

    add tests

--- src/main/kotlin/tests/test1/Vector.kt
+++ src/main/kotlin/tests/test1/Vector.kt
@@ -1,21 +1,18 @@
 package tests.test1
 
-interface ArithmeticAvailable<T> {
-    val zero: T
-    val element: T
+interface ArithmeticAvailable<T : ArithmeticAvailable<T>> {
     fun isZero(): Boolean
     operator fun plus(other: T): T
-    operator fun minus(other:T): T
+    operator fun minus(other: T): T
     operator fun times(other: T): T
-
 }
 
-class IntVectorElements(override val element: Int) : ArithmeticAvailable<Int> {
-    override val zero = 0
-    override fun isZero(): Boolean = element == zero
-    override fun plus(other: Int): Int = element + other
-    override fun minus(other: Int): Int = element - other
-    override fun times(other: Int): Int = element * other
+class IntVectorElements(val element: Int) : ArithmeticAvailable<IntVectorElements> {
+    override fun isZero(): Boolean = element == 0
+    override fun plus(other: IntVectorElements) = IntVectorElements(element + other.element)
+    override fun minus(other: IntVectorElements) = IntVectorElements(element - other.element)
+    override fun times(other: IntVectorElements) = IntVectorElements(element * other.element)
+    override fun toString() = "$element"
 }
 
 class Vector<T : ArithmeticAvailable<T>>(val elements: List<T>) {
@@ -37,14 +34,10 @@ class Vector<T : ArithmeticAvailable<T>>(val elements: List<T>) {
     operator fun times(other: Vector<T>): T {
         require(length == other.length) { "Vectors must have equal length." }
         val newVector = elements.mapIndexed { index, element -> element * other.elements[index] }
-        var result = elements[0].zero
-        newVector.forEach { result += it.element }
-        return result
+        return newVector.reduce { acc, element -> acc + element }
     }
 
     val isZeroVector: Boolean
         get() = elements.all { it.element.isZero()}
 
 }
-
-
